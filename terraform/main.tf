873804e383036fa4d907eff03f1565de2972f0ff
ashrafib <ashraf.ibrahim@ista.de>
2021-01-12 17:47:31 +0100

    Pipe

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   features {}
 }
 terraform {
   backend "azurerm" {
-    resource_group_name  =  "Quality"
+    resource_group_name  = "Quality"
     storage_account_name = "quality"
     container_name       = "qualitycontainer"
     key                  =  "terraform.tfstate"
