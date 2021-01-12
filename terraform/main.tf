fe588e963caa54d59bd87a35e24058dda15724f8
AshrafIb <54529492+AshrafIb@users.noreply.github.com>
2021-01-12 09:12:55 +0100

    Infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,19 +7,20 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    resource_group_name  =  "Quality"
+    storage_account_name = "quality"
+    container_name       = "qualitycontainer"
+    key                  =  "terraform.tfstate"
+    access_key           = "QBvo+oVykRtxgh3PX9yEzS0WCMi1B3Sy3NGxzr0e7+19a/5tUPAMAolAWxAkFZcmjPLQ8W0ldY7CKrIQ5nGz0w=="
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
+  source               = "./modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
 module "network" {
-  source               = "../../modules/network"
+  source               = "./modules/network"
   address_space        = "${var.address_space}"
   location             = "${var.location}"
   virtual_network_name = "${var.virtual_network_name}"
@@ -30,7 +31,7 @@ module "network" {
 }
 
 module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
+  source           = "./modules/networksecuritygroup"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
@@ -39,14 +40,14 @@ module "nsg-test" {
   address_prefix_test = "${var.address_prefix_test}"
 }
 module "appservice" {
-  source           = "../../modules/appservice"
+  source           = "./modules/appservice"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
   resource_group   = "${module.resource_group.resource_group_name}"
 }
 module "publicip" {
-  source           = "../../modules/publicip"
+  source           = "./modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,15 +1,15 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "939bb7d7-6909-4049-929c-1df590b9f273"
+client_id = "1c4ac71a-3edf-4377-9f56-2f631c408623"
+client_secret = "3JRxYwyqIB_V-6R8yl5zaB77cVCIDVR-px"
+tenant_id = "e9f4b799-4c58-4783-92c3-75ff1e37dd9c"
 
 # Resource Group/Location
-location = ""
-resource_group = ""
-application_type = ""
+location = "westeurope"
+resource_group = "Quality_Project"
+application_type = "qualityproject"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "finalproject"
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
